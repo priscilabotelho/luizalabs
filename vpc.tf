ea298597fd11072a92455f7f60e7d421ff570481
Priscila Botelho <priscilafnbotelho@gmail.com>
2024-06-29 15:56:32 -0300

    add vpc

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,9 @@
 variable "project_id" {
-  description = "Project ID"
+  description = "project id"
 }
 
 variable "region" {
-  description = "Region"
+  description = "region"
 }
 
 provider "google" {
@@ -11,11 +11,13 @@ provider "google" {
   region  = var.region
 }
 
+# VPC
 resource "google_compute_network" "vpc" {
   name                    = sensitive("${var.project_id}-vpc")
-  auto_create_subnetworks = false
+  auto_create_subnetworks = "false"
 }
 
+# Subnet
 resource "google_compute_subnetwork" "subnet" {
   name          = sensitive("${var.project_id}-subnet")
   region        = var.region
